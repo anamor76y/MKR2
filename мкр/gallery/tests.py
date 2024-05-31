4081c0dd952c3909fd593b3c3f954a68057cb559
anamor76y <anna.moroz@knu.ua>
2024-05-31 11:49:01 +0300

    tests

--- мкр/gallery/tests.py
+++ мкр/gallery/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Image
+
+class GalleryViewTest(TestCase):
+    def test_gallery_view(self):
+        client = Client()
+        response = client.get(reverse('gallery_view'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+
+class ImageDetailViewTest(TestCase):
+    def setUp(self):
+        self.image = Image.objects.create(title='Test Image', description='Test Description')
+
+    def test_image_detail_view(self):
+        client = Client()
+        response = client.get(reverse('image_detail', args=(self.image.id,)))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'image_detail.html')
