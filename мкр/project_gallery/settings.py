2ac84cacaf9acda89df47ae92f3c3063a5fde099
anamor76y <anna.moroz@knu.ua>
2024-05-31 11:55:05 +0300

    database

--- мкр/project_gallery/settings.py
+++ мкр/project_gallery/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'project_gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MKR',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
+
+
 
 
 # Password validation
